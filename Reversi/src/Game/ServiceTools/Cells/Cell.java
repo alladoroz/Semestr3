3b725957d4a378f4ab79f00fbaaed9936734c7d3
alladoroz <alla.dorozhkina@gmail.com>
2014-03-24 12:40:52 +0000

    Удаление лишних файлов

--- Reversi/src/Game/ServiceTools/Cells/Cell.java
+++
@@ -1,9 +0,0 @@
-package Game.ServiceTools.Cells;
-
-public abstract class Cell {
-    protected boolean isWall;
-
-    public boolean isWall(){
-        return isWall;
-    }
-}
--- Reversi/src/Reversi.java
+++
@@ -1,8 +0,0 @@
-import Game.WindowObjects.MainForm;
-
-public class Reversi {
-
-    public static void main(String[] args) {
-        new MainForm();
-    }
-}
